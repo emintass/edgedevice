4252e4def619a943858e6fc36c8dc6da83e9bf7f
Mehmet Emin Taş <emin.tas@metu.edu.tr>
2025-08-01 17:24:04 +0300

    small ch

--- sayac_sorgu.py
+++ sayac_sorgu.py
@@ -4,9 +4,9 @@ ser = serial.Serial(
     port='/dev/ttyUSB0',  # adaptörün portu
     baudrate=300,
     bytesize=serial.SEVENBITS,
-    parity=serial.PARITY_NONE,
+    parity=serial.PARITY_EVEN,
     stopbits=serial.STOPBITS_ONE,
-    timeout=1
+    timeout=2.0
 )
 
 try:
